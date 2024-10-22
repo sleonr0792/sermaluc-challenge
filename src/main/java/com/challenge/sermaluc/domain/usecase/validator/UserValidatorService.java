1b2eb32f5945f6206eab45c1d3c8ba50b9ba3370
system-u <sleonr0792@gmail.com>
2024-10-21 23:23:14 -0500

    feat: se agregó un servicio para las validaciones de negocio de la creación de usuarios.

--- src/main/java/com/challenge/sermaluc/domain/usecase/validator/UserValidatorService.java
+++ src/main/java/com/challenge/sermaluc/domain/usecase/validator/UserValidatorService.java
@@ -1,2 +1,7 @@
-package com.challenge.sermaluc.domain.usecase.validator;public interface UserValidatorService {
+package com.challenge.sermaluc.domain.usecase.validator;
+
+public interface UserValidatorService {
+    void throwIfEmailInvalid(String email);
+    void throwIfUserExits(String email);
+    void throwIfPasswordInvalid(String password);
 }
