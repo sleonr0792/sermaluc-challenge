2705a24f8815d2a03b01aa9e2cac20228d1b8eca
system-u <sleonr0792@gmail.com>
2024-10-21 23:39:37 -0500

    fix: se modificó el estado de la excepcion de negocio. Se agregaron más excepciones para cada estado de error

--- src/main/java/com/challenge/sermaluc/config/exceptions/ExceptionHandlerRestController.java
+++ src/main/java/com/challenge/sermaluc/config/exceptions/ExceptionHandlerRestController.java
@@ -8,27 +8,45 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.client.HttpClientErrorException;
 
 @ControllerAdvice
 @Slf4j
 public class ExceptionHandlerRestController {
-    @ExceptionHandler(value = Exception.class)
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public ResponseEntity<Object> exception(Exception exception) {
-        log.error("[Controller][Error] exception Ocurrio un error durante la ejecucion: ",
-                exception);
-        return new ResponseEntity<>("", HttpStatus.INTERNAL_SERVER_ERROR);
-    }
 
     @ExceptionHandler(value = BusinessException.class)
-    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity<Object> businessException(BusinessException exception) {
-        log.error("[Controller][Error] exception Ocurrio un error durante la ejecucion: ",
+        log.error("[Controller][Error] exception Ocurrio un error durante la ejecucion [businessException]: ",
                 exception);
-        return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY)
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                 .body(MessageError.builder()
                         .mensaje(exception.getMessage())
                         .build());
+    }
+
+    @ExceptionHandler(value = HttpClientErrorException.Forbidden.class)
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    public ResponseEntity<Object> noAccesibleRecurso(Exception exception) {
+        log.error("[Controller][Error] exception Ocurrio un error durante la ejecucion  [noAccesibleRecurso]: ",
+                exception);
+        return new ResponseEntity<>("No se puede acceder a este recurso", HttpStatus.FORBIDDEN);
+    }
+
+    @ExceptionHandler(value = HttpClientErrorException.NotFound.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseEntity<Object> recursoNoEncontrado(Exception exception) {
+        log.error("[Controller][Error] exception Ocurrio un error durante la ejecucion [recursoNoEncontrado]: ",
+                exception);
+        return new ResponseEntity<>("Recurso no encontrado", HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(value = Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ResponseEntity<Object> genericException(Exception exception) {
+        log.error("[Controller][Error] exception Ocurrio un error durante la ejecucion [genericException]: ",
+                exception);
+        return new ResponseEntity<>("Ha Ocurrido un error Inesperado", HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @Builder
